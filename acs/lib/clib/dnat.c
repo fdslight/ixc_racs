86b52f4a8ecf183516d41b048d233a89697c2a71
freek <freekai@outlook.com>
2022-05-15 11:44:11 +0800

    ...

--- acs/lib/clib/dnat.c
+++ acs/lib/clib/dnat.c
@@ -207,8 +207,8 @@ int ixc_dnat_rule_add(const unsigned char *_id,const unsigned char *left_addr,co
         memcpy(rule->right_addr,right_addr,4);
     }
 
-    PRINT_IP(rule->left_addr);
-    PRINT_IP(rule->right_addr);
+    PRINT_IP(" ",rule->left_addr);
+    PRINT_IP(" ",rule->right_addr);
     rule->refcnt=2;
 
     return 0;
